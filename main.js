1ad8260a0fea2d680bb2f6539273fd29ba96139d
Bartosz <126903255+Brontiq18@users.noreply.github.com>
2023-03-04 11:34:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 function walidacja_danych(){
     var wpis=1;
-    var braki="";
+    var braki=" ";
     if (document.getElementById("imie").value == ""){
         braki += "imie\n"
         wpis=0;
     }
-    if (document.getElementById("nazwisko").value == ""){
-        braki += "nazwisko\n";
+    if (document.getElementById("email").value == ""){
+        braki += "email\n";
+        wpis=0;
+    }
+    if (document.getElementById("ocena").value == ""){
+        braki += "ocena\n";
+        wpis=0;
+    }
+    if (document.getElementById("data").value == ""){
+        braki += "data\n";
         wpis=0;
     }
     if (wpis)
     alert("obowiązkowe pola zostały wypelnione. ");
-    else alert("Nie wypełniono obowiążkowych pól:\n" + braki);  
+    else alert("Nie wypełniono obowiązkowych pól:\n" + braki);  
 }
